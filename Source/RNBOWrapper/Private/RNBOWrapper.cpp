7d0389768031a0c7d8c45a711d68e6d5e2fa160f
Alex Norman <xnor@cycling74.com>
2023-08-13 08:18:44 -0700

    attempting to create FTransport data type
    fails with BindReadVertex

--- Source/RNBOWrapper/Private/RNBOWrapper.cpp
+++ Source/RNBOWrapper/Private/RNBOWrapper.cpp
@@ -4,6 +4,8 @@
 #include "MetasoundFrontendRegistries.h"
 #include "MetasoundSampleCounter.h"
 
+#include "MetasoundDataTypeRegistrationMacro.h"
+
 #define LOCTEXT_NAMESPACE "FRNBOWrapperModule"
 
 void FRNBOWrapperModule::StartupModule()
@@ -18,6 +20,32 @@ void FRNBOWrapperModule::ShutdownModule()
 	// we call this function before unloading the module.
 }
 
+
+namespace Metasound {
+
+	// Disable constructor pins of triggers 
+	template<>
+	struct TEnableConstructorVertex<FTransport>
+	{
+		static constexpr bool Value = false;
+	};
+
+	// Disable arrays of triggers
+	template<>
+	struct TEnableAutoArrayTypeRegistration<FTransport>
+	{
+		static constexpr bool Value = false;
+	};
+
+	// Disable auto-conversions based on FTransport implicit converters
+	template<typename ToDataType>
+	struct TEnableAutoConverterNodeRegistration<FTransport, ToDataType>
+	{
+		static constexpr bool Value = !std::is_arithmetic<ToDataType>::value;
+	};
+
+  REGISTER_METASOUND_DATATYPE(FTransport, "Transport", ::Metasound::ELiteralType::Boolean)
+}
 
 namespace 
 {
@@ -57,6 +85,7 @@ namespace
 
 					FOutputVertexInterface outputs;
 					outputs.Add(TOutputDataVertex<FTime>(METASOUND_GET_PARAM_NAME_AND_METADATA(ParamTransportBeatTime)));
+					outputs.Add(TOutputDataVertex<FTransport>(METASOUND_GET_PARAM_NAME_AND_METADATA(ParamTransport)));
 
 					FVertexInterface Interface(inputs, outputs);
 
@@ -81,7 +110,8 @@ namespace
 					const FInputVertexInterface& InputInterface,
 					FBuildErrorArray& OutErrors)
 				:
-				BlockSize(InSettings.GetNumFramesPerBlock())
+         Foo()
+				, BlockSize(InSettings.GetNumFramesPerBlock())
 				, SampleRate(static_cast<double>(InSettings.GetSampleRate()))
 				, BlockPeriod(InSettings.GetNumFramesPerBlock() / InSettings.GetSampleRate())
 				, PeriodMul(8.0 / 480.0 * InSettings.GetNumFramesPerBlock() / InSettings.GetSampleRate())
@@ -92,19 +122,17 @@ namespace
 			{
 			}
 
-			virtual FDataReferenceCollection GetInputs()  const override {
-				FDataReferenceCollection InputDataReferences;
-				InputDataReferences.AddDataReadReference(METASOUND_GET_PARAM_NAME(ParamTransportBPM), TransportBPM);
-				InputDataReferences.AddDataReadReference(METASOUND_GET_PARAM_NAME(ParamTransportRun), TransportRun);
-				return InputDataReferences;
-			}
+      virtual void BindInputs(FInputVertexInterfaceData& InOutVertexData) override
+      {
+        InOutVertexData.BindReadVertex(METASOUND_GET_PARAM_NAME(ParamTransportBPM), TransportBPM);
+        InOutVertexData.BindReadVertex(METASOUND_GET_PARAM_NAME(ParamTransportRun), TransportRun);
+      }
 
-			virtual FDataReferenceCollection GetOutputs() const override {
-				FDataReferenceCollection OutputDataReferences;
-				OutputDataReferences.
-                AddDataReadReference(METASOUND_GET_PARAM_NAME(ParamTransportBeatTime), TransportBeatTime);
-				return OutputDataReferences;
-			}
+      virtual void BindOutputs(FOutputVertexInterfaceData& InOutVertexData) override
+      {
+        InOutVertexData.BindReadVertex(METASOUND_GET_PARAM_NAME(ParamTransportBeatTime), TransportBeatTime);
+        InOutVertexData.BindReadVertex(METASOUND_GET_PARAM_NAME(ParamTransport), Foo);
+      }
 
 			void Execute() {
 				if (*TransportRun) {
@@ -115,10 +143,13 @@ namespace
 			}
 
 		private:
+      FTransport Foo;
+
 			FSampleCount BlockSize;
 			FSampleRate SampleRate;
 			FTime BlockPeriod;
 			double PeriodMul;
+
 
 			FTimeWriteRef TransportBeatTime;
 			FFloatReadRef TransportBPM;
--- Source/RNBOWrapper/Public/RNBOWrapper.h
+++ Source/RNBOWrapper/Public/RNBOWrapper.h
@@ -6,6 +6,11 @@
 #include "Modules/ModuleManager.h"
 #include "MetasoundFacade.h"
 #include "MetasoundParamHelper.h"
+#include "MetasoundDataReferenceMacro.h"
+#include "MetasoundTime.h"
+
+#include "MetasoundExecutableOperator.h"
+#include "MetasoundOperatorSettings.h"
 
 class FRNBOWrapperModule : public IModuleInterface
 {
@@ -35,5 +40,27 @@ namespace RNBOWrapper {
 		METASOUND_PARAM(ParamTransportRun, "Transport Run", "The run state of the transport.")
 		METASOUND_PARAM(ParamTransportNum, "Transport Numerator", "The transport time signature numerator.")
 		METASOUND_PARAM(ParamTransportDen, "Transport Denominator", "The transport time signature denominator.")
+		METASOUND_PARAM(ParamTransport, "Transport", "The transport.")
 #undef LOCTEXT_NAMESPACE
 }
+
+namespace Metasound {
+  //TODO  METASOUNDFRONTEND_API ?
+  class METASOUNDFRONTEND_API FTransport {
+    public:
+      FTransport() : BeatTime(0.0) { }
+			explicit FTransport(const FOperatorSettings& InSettings, bool bShouldRun) : 
+        BeatTime(0.0),
+        Run(bShouldRun) {
+      }
+    private:
+      FTime BeatTime;
+      bool Run = false;
+      float BPM = 120.0f;
+      int32 TimeSigNum = 4;
+      int32 TimeSigDen = 4;
+  };
+  //XXX what about the METASOUNDFRONTEND_API ?
+	DECLARE_METASOUND_DATA_REFERENCE_TYPES(FTransport, METASOUNDFRONTEND_API, FTransportTypeInfo, FTransportReadRef, FTransportWriteRef);
+}
+
