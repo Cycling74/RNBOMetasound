cc398e0c40323d37df688626dfac2b5ad5cd71fe
Alex Norman <xnor@cycling74.com>
2024-05-31 05:57:47 -0700

    re-enable note off velocity

--- Source/RNBOMetasound/Private/RNBOMakeNote.cpp
+++ Source/RNBOMetasound/Private/RNBOMakeNote.cpp
@@ -17,7 +17,7 @@ namespace {
 METASOUND_PARAM(ParamMakeNoteTrig, "Trigger", "Create a note.")
 METASOUND_PARAM(ParamMakeNoteNote, "Note", "The note number (0-127).")
 METASOUND_PARAM(ParamMakeNoteVel, "Velocity", "The note velocity (1-127).")
-//METASOUND_PARAM(ParamMakeNoteOffVel, "Off Velocity", "The note-off velocity (0-127).")
+METASOUND_PARAM(ParamMakeNoteOffVel, "Off Velocity", "The note-off velocity (0-127).")
 METASOUND_PARAM(ParamMakeNoteChan, "Channel", "The note channel (0-15).")
 METASOUND_PARAM(ParamMakeNoteDur, "Duration", "The note duration (seconds).")
 
@@ -58,7 +58,7 @@ class FMakeNoteOperator : public TExecutableOperator<FMakeNoteOperator>
 
             inputs.Add(TInputDataVertex<int32>(METASOUND_GET_PARAM_NAME_AND_METADATA(ParamMakeNoteNote), 63));
             inputs.Add(TInputDataVertex<int32>(METASOUND_GET_PARAM_NAME_AND_METADATA(ParamMakeNoteVel), 100));
-            //inputs.Add(TInputDataVertex<int32>(METASOUND_GET_PARAM_NAME_AND_METADATA(ParamMakeNoteOffVel), 0));
+            inputs.Add(TInputDataVertex<int32>(METASOUND_GET_PARAM_NAME_AND_METADATA(ParamMakeNoteOffVel), 0));
             inputs.Add(TInputDataVertex<int32>(METASOUND_GET_PARAM_NAME_AND_METADATA(ParamMakeNoteChan), 0));
 
             inputs.Add(TInputDataVertex<FTime>(METASOUND_GET_PARAM_NAME_AND_METADATA(ParamMakeNoteDur), 0.05f));
@@ -94,7 +94,7 @@ class FMakeNoteOperator : public TExecutableOperator<FMakeNoteOperator>
 
         , NoteNum(InputCollection.GetOrCreateDefaultDataReadReference<int32>(METASOUND_GET_PARAM_NAME(ParamMakeNoteNote), InSettings))
         , NoteVel(InputCollection.GetOrCreateDefaultDataReadReference<int32>(METASOUND_GET_PARAM_NAME(ParamMakeNoteVel), InSettings))
-        //, NoteOffVel(InputCollection.GetOrCreateDefaultDataReadReference<int32>(METASOUND_GET_PARAM_NAME(ParamMakeNoteOffVel), InSettings))
+        , NoteOffVel(InputCollection.GetOrCreateDefaultDataReadReference<int32>(METASOUND_GET_PARAM_NAME(ParamMakeNoteOffVel), InSettings))
         , NoteChan(InputCollection.GetOrCreateDefaultDataReadReference<int32>(METASOUND_GET_PARAM_NAME(ParamMakeNoteChan), InSettings))
 
         , NoteDur(InputCollection.GetOrCreateDefaultDataReadReference<FTime>(METASOUND_GET_PARAM_NAME(ParamMakeNoteDur), InSettings))
@@ -108,7 +108,7 @@ class FMakeNoteOperator : public TExecutableOperator<FMakeNoteOperator>
         InOutVertexData.BindReadVertex(METASOUND_GET_PARAM_NAME(ParamMakeNoteTrig), Trigger);
         InOutVertexData.BindReadVertex(METASOUND_GET_PARAM_NAME(ParamMakeNoteNote), NoteNum);
         InOutVertexData.BindReadVertex(METASOUND_GET_PARAM_NAME(ParamMakeNoteVel), NoteVel);
-        //InOutVertexData.BindReadVertex(METASOUND_GET_PARAM_NAME(ParamMakeNoteOffVel), NoteOffVel);
+        InOutVertexData.BindReadVertex(METASOUND_GET_PARAM_NAME(ParamMakeNoteOffVel), NoteOffVel);
         InOutVertexData.BindReadVertex(METASOUND_GET_PARAM_NAME(ParamMakeNoteChan), NoteChan);
         InOutVertexData.BindReadVertex(METASOUND_GET_PARAM_NAME(ParamMakeNoteDur), NoteDur);
     }
@@ -126,16 +126,16 @@ class FMakeNoteOperator : public TExecutableOperator<FMakeNoteOperator>
 
         uint32 id = 0; //XXX do we need some other ID?
         if (num > 0) {
-            const auto note = static_cast<int32>(std::clamp(*NoteNum, 0, 127));
-            const auto vel = static_cast<int32>(std::clamp(*NoteVel, 1, 127));
-            //const auto offvel = static_cast<int32>(std::clamp(*NoteOffVel, 0, 127));
-            const auto chan = static_cast<int32>(std::clamp(*NoteChan, 0, 15));
+            const auto note = static_cast<int8>(std::clamp(*NoteNum, 0, 127));
+            const auto vel = static_cast<int8>(std::clamp(*NoteVel, 1, 127));
+            const auto offvel = static_cast<int8>(std::clamp(*NoteOffVel, 0, 127));
+            const auto chan = static_cast<int8>(std::clamp(*NoteChan, 0, 15));
             const int32 dur = std::max(static_cast<int32>(ceil(SampleRate.GetSeconds() * NoteDur->GetSeconds())), 1);
 
             for (auto i = 0; i < num; i++) {
                 auto start = (*Trigger)[i];
                 HarmonixMetasound::FMidiStreamEvent noteon(id, FMidiMsg::CreateNoteOn(chan, note, vel));
-                HarmonixMetasound::FMidiStreamEvent noteoff(id, FMidiMsg::CreateNoteOff(chan, note));
+                HarmonixMetasound::FMidiStreamEvent noteoff(id, FMidiMsg(chan | 0x80, note, offvel));
                 noteoff.BlockSampleFrameIndex = dur;
                 MIDIOut->AddMidiEvent(noteon);
                 MIDIOut->AddMidiEvent(noteoff);
@@ -150,7 +150,7 @@ class FMakeNoteOperator : public TExecutableOperator<FMakeNoteOperator>
 
     FInt32ReadRef NoteNum;
     FInt32ReadRef NoteVel;
-    //FInt32ReadRef NoteOffVel;
+    FInt32ReadRef NoteOffVel;
     FInt32ReadRef NoteChan;
 
     FTimeReadRef NoteDur;
