a97958496f4f6ffdaf14961b8fd242d260780e10
Alex Van Gils <alex@cycling74.com>
2025-12-10 15:53:33 -0500

    update MS registration macros, see #66

--- Source/RNBOMetasound/Private/RNBOMetasound.cpp
+++ Source/RNBOMetasound/Private/RNBOMetasound.cpp
@@ -3,17 +3,19 @@
 #include "RNBOMetasound.h"
 #include "RNBOTransport.h"
 #include "MetasoundFrontendRegistries.h"
+#include "MetasoundFrontendModuleRegistrationMacros.h"
 
 void FRNBOMetasoundModule::StartupModule()
 {
     // This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
-    FMetasoundFrontendRegistryContainer::Get()->RegisterPendingNodes();
+    METASOUND_REGISTER_ITEMS_IN_MODULE
 }
 
 void FRNBOMetasoundModule::ShutdownModule()
 {
     // This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
     // we call this function before unloading the module.
+    METASOUND_UNREGISTER_ITEMS_IN_MODULE
 }
 
 IMPLEMENT_MODULE(FRNBOMetasoundModule, RNBOMetasound)
